bbc69dbe5f86ae33b9b52a98d75c253c6b94831e
Olga Velichko <Ovelichko@Paylocity.com>
2022-06-27 21:05:17 -0500

    reorgonize files

---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,12 @@
+namespace Catalog.Entities
+{
+
+    public class Item
+    {
+        public Guid Id { get; init; }
+        public String Name { get; init; }
+        public decimal Price { get; set; }
+
+        public DateTimeOffset CreateDate { get; init; }
+    }
+}
